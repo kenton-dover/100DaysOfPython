761581df7601571a80ab90a4773f8ca432e0c385
Kenton Dover <doverkenton@gmail.com>
2024-05-14 16:38:06 -0400

    ading changes

--- day10/calculator.py
+++ day10/calculator.py
@@ -1,6 +1,6 @@
 import os
 clear = lambda: os.system('cls')
-
+from art import logo
 
 def add(n1: float, n2: float):
     return n1 + n2
@@ -21,14 +21,33 @@ operations ={
     "/": divide,
 }
 
-num1 = int(input("Whats the first number: "))
-num2 = int(input("Whats the second number: "))
+def calculator():
+    print(logo)
+    num1 = float(input("Whats the first number: "))
 
-for op in operations:
-    print(op)
-    
-symbol = input("Pick an peration from the lines above: ")
+    for op in operations:
+        print(op)
+        
 
-answer = operations[symbol](num1, num2)
+    should_continue = True
 
-print(f"{num1} {symbol} {num2} = {answer}")
+    while should_continue:
+        symbol = input("Pick an peration from the lines above: ")
+        num2 = float(input("Whats the next number: "))
+        
+
+
+        
+        
+
+        answer = operations[symbol](num1, num2)
+
+        print(f"{num1} {symbol} {num2} = {answer}")
+        
+        if input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+            
+calculator()
